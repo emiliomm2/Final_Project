bb394901490d0eff2f14ea0dd5d29f96742bc0a9
emilio <emiliomm3@gmail.com>
2024-03-27 14:11:54 +0100

    Proyecto en web terminado

--- php/descargar_notas.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $nombreArchivo = 'notas.txt'; 
-    $rutaArchivo = '../notas/' . $nombreArchivo; 
-
-
-    header('Content-Type: application/octet-stream');
-    header('Content-Disposition: attachment; filename="' . $nombreArchivo . '"');
-    readfile($rutaArchivo);
-?>
